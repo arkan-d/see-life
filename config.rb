61d8806298c1f7bb5e47c0966ed0698a0d80b2eb
arkan-d <profiks.1994@mail.ru>
2015-10-08 14:27:27 -0700

    basic structure
    File/folder structure

---
+++ config.rb
@@ -0,0 +1,10 @@
+project_type = :stand_alone
+http_path= "../"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img/"
+javascripts_dir = "js"
+fonts_dir = "fonts/"
+line_comments = false
+output_style = :compressed
+relative_assets = true
